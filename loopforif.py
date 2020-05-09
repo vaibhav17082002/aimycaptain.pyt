897e9a13dd1df1bd8d3c3f48f3abc7f171d1bb7b
vaibhav17082002 <64905627+vaibhav17082002@users.noreply.github.com>
2020-05-09 19:11:53 +0530

    Update loopforif.py

--- loopforif.py
+++ loopforif.py
@@ -2,7 +2,7 @@ list1=[12,-7,5,64,-14]
 list2=[12,14,-95,3]
 for x in list1:
     if x>0:
-        print(x)
+        print("list1[",x,"]")
 for x in list2:
     if x>0:
-        print(x)
+        print("list2[",x,"]")

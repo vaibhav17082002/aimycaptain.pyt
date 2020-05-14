13e6b72441589c785661b74185c7ab519fafa5af
vaibhav17082002 <64905627+vaibhav17082002@users.noreply.github.com>
2020-05-14 17:30:04 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,23 @@
+list1=[1,2,34,23,'hello','lol']
+list2=[93,73,98,"yo"]
+
+list1.append('rofl')
+list2.append('honey singh')
+print(list1)
+print(list2)
+ 
+    
+tupple1=(1,2,3,98,45)
+tupple1[3]
+tupple1[0:4]
+tupple1[0:]
+
+
+dict1={
+    "lol":"laugh",
+    "rofl":"laugh loud",
+    "god":"one"
+}
+print(dict1)
+del dict1["lol"]
+print(dict1)
